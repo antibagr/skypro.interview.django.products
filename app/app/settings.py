45a680f1828c6d60976fbe5c4b80c5badb354a7b
Anton Bagrianov <rudiemeant@gmail.com>
2023-09-30 05:28:24 +0400

    Fix code to pass linters

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-chw!h4ulgq)j4=agc%@yhi9fj8^4sqn2gbiq38!!e+fact9f-7"
+SECRET_KEY = "django-insecure-chw!h4ulgq)j4=agc%@yhi9fj8^4sqn2gbiq38!!e+fact9f-7"  # nosec
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  # nosec
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
